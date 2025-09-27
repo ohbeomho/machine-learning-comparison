f799ed5e2f7f6a1d05125dbf9e40d32dc3e28e1a
ohbeomho <ohbeomho0322@gmail.com>
2025-09-27 16:57:36 +0900

    Missing value checked

---
+++ korea_energy.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+# 결측치 없음
+energy_data = pd.read_csv("./korea-energy/EnergyUsage_bySector.csv")
---
+++ korea_pollution.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+# 결측치 없음
+pollution_data = pd.read_csv("./korea-pollution/south-korean-pollution-data.csv")
