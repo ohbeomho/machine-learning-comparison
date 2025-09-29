4d4c85cb268453340addbb2a13f145727e05baca
ohbeomho <ohbeomho0322@gmail.com>
2025-09-29 10:42:12 +0900

    에측, 학습에 사용할 데이터

--- diabetes.py
+++ diabetes.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 # 결측치 없음
-diabetes_data = pd.read_csv("./diabetes/diabetes_012_health_indicators_BRFSS2015.csv")
+diabetes_data = pd.read_csv('./diabetes/diabetes_012_health_indicators_BRFSS2015.csv')
+target_col = 'Diabetes_012' # 예측할 데이터
--- gdp.py
+++ gdp.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
-gdp_data = pd.read_csv("./gdp/gdp.csv")
+gdp_data = pd.read_csv('./gdp/gdp.csv')
 # 결측치 제거
 gdp_data.dropna(axis=0, inplace=True)
+# 학습에 사용할 데이터 (1960~2010년)
+train_col = range(1960, 2011)
+# 에측할 데이터 (2011~2020년)
+target_col = range(2011, 2021)
