41b902454d25b52f2b902525c8b668d08329a46a
ohbeomho <ohbeomho0322@gmail.com>
2025-10-05 20:08:06 +0900

    Korea pollution machine learning
    This data is definately not for linear regression lol

--- korea_pollution.py
+++ korea_pollution.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 import koreanize_matplotlib
 
 # 결측치 없음
@@ -14,34 +15,57 @@ pollution_data = pollution_data[pollution_data['Measurement date'].str[-5:-3] ==
 
 
 # 이상치 제거
-def remove_outlier(column):
+def remove_outliers(column):
     global pollution_data
+    Q1 = pollution_data[column].quantile(0.25)
+    Q3 = pollution_data[column].quantile(0.75)
+    IQR = Q3 - Q1
 
-    # 200 이상의 데이터 제거
-    pollution_data = pollution_data[pollution_data[column] < 200]
+    pollution_data = pollution_data[
+        (pollution_data[column] >= Q1 - 2 * IQR)
+        & (pollution_data[column] <= Q3 + 2 * IQR)
+    ]
 
 
-remove_outlier('PM2.5')
-remove_outlier('PM10')
+remove_outliers('PM2.5')
 
 # 날짜에 따라 정렬
 pollution_data.sort_values('Measurement date', inplace=True, ignore_index=True)
 dates = pollution_data['Measurement date']
 pm25 = pollution_data['PM2.5']
-pm10 = pollution_data['PM10']
+
+test_size = int(0.2 * len(dates))
+train_size = len(dates) - test_size
+
+poly = PolynomialFeatures(degree=3)
+train_poly = poly.fit_transform(np.arange(train_size).reshape(-1, 1))
+test_poly = poly.transform(np.arange(train_size, train_size + test_size).reshape(-1, 1))
 
 model = LinearRegression()
-# TODO: Machine learning
+model.fit(
+    train_poly,
+    np.array(pm25[:train_size]).reshape(-1, 1),
+)
+pm25_pred = model.predict(test_poly)
 
 ax = plt.axes()
 
-ax.plot(dates, pm10)
-ax.plot(dates, pm25)
-ax.legend(('PM10', 'PM2.5'))
+ax.plot(dates[:train_size], pm25[:train_size])
+ax.plot(dates[train_size:], pm25[train_size:])
+ax.plot(dates[train_size:], pm25_pred)
+
+ax.legend(
+    (
+        '학습 데이터',
+        '실제 값',
+        '예측 데이터',
+    )
+)
+
 ax.tick_params(axis='x', which='minor', labelbottom=False, top=False, bottom=False)
 ax.tick_params(axis='x', rotation=90)
 ax.xaxis.set_major_locator(MultipleLocator(10))
 
 plt.xlabel('날짜')
-plt.ylabel('PM2.5/PM10 (μg/m3)')
+plt.ylabel('PM2.5 (μg/m3)')
 plt.show()
